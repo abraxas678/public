effb37f4dce46a7739251b88201542637cb91d65
abraxas678 <abraxas678@gmail.com>
2023-07-09 16:09:23 +0200

    Update setup_keybase.sh

--- setup_keybase.sh
+++ setup_keybase.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export XDG_RUNTIME_DIR=""
-curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb; 
-sudo apt install ./keybase_amd64.deb -y; 
-run_keybase; 
+curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
+sudo apt install ./keybase_amd64.deb -y
+run_keybase
 keybase login
