b79d6fa7a91f89abba19d2b649c9581a481dc2cb
abraxas678 <abraxas678@gmail.com>
2023-08-06 22:25:10 +0200

    Update pueue_setup.sh

--- pueue_setup.sh
+++ pueue_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir $HOME/tmp >/dev/null 2>/dev/null
 cd $HOME/tmp
-VERSION=$(curl -Ls https://github.com/Nukesor/pueue/releases/latest | grep "<title>Release " | sed 's/.*<title>Release //' | sed 's/┬À Nukesor.*//' | sed 's/ //g')
+VERSION=$(curl -Ls https://github.com/Nukesor/pueue/releases/latest | grep "<title>Release " | sed 's/.*<title>Release //' | sed 's/Nukesor.*//' | sed 's/ //g')
 wget "https://github.com/Nukesor/pueue/releases/download/$VERSION/pueued-linux-x86_64"
 wget "https://github.com/Nukesor/pueue/releases/download/$VERSION/pueue-linux-x86_64"
 mv pueued-linux-x86_64 pueued
