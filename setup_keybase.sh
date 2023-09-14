5264dc1160518e43c95018735b269abbe491fef1
abraxas678 <abraxas678@gmail.com>
2023-09-14 23:29:36 +0200

    Update setup_keybase.sh

--- setup_keybase.sh
+++ setup_keybase.sh
@@ -5,6 +5,10 @@ then
     sudo apt-get install -y curl
 fi
 export XDG_RUNTIME_DIR=""
+read -p "via docker? >> " -n ANS
+if [[ $ANS = "y" ]]; then
+
+else
 [[ ! -f keybase_amd64.deb ]] && curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
 sudo apt update
 sudo apt install ./keybase_amd64.deb -y
@@ -19,3 +23,4 @@ echo
 read -p "KEYBASE USER: >> " MYUSER
 cd $HOME
 git clone keybase://private/$MYUSER/server_setup
+fi
