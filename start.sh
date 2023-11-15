bd18be0e5f2e88e38433f24bdc5ec431f631fd23
abraxas678 <abraxas678@gmail.com>
2023-11-15 01:43:20 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -26,16 +26,17 @@ fi
 [[ $(twingate status) != *"online"* ]] && timeout 10 /usr/bin/twingate-notifier console
 
 # Mount SNAS setup
+mkdir -p /home/mnt/snas/sync
 sudo mkdir /home/mnt/snas/setup -p
 sudo chown $USER: -R /home/mnt/snas
+sudo chmod 777 /home/mnt/snas -R
 echo
 echo mount nfs version 3
 echo; sleep 3
-mkdir -p /home/mnt/snas/sync
 [[ ! -f /home/mnt/snas/sync/MOUNT_CHECK ]] && sudo mount -t nfs -o vers=3 192.168.178.35:/volume2/sync /home/mnt/snas/sync
 [[ ! -f /home/mnt/snas/setup/MOUNT_CHECK ]] && sudo mount -t nfs -o vers=3 192.168.178.35:/volume1/setup /home/mnt/snas/setup
-sudo chown $USER: -R /home/mnt/snas
-sudo chmod 777 /home/mnt/snas -R
+sudo chown $USER: -R /home/mnt/snas/setup
+sudo chmod 777 /home/mnt/snas/setup -R
 
 x=0
 while [[ $x = "0" ]]; do
