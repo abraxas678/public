bdab58f1dd8f3c3eed7fdd34738be8b1f8286819
abraxas678 <abraxas678@gmail.com>
2023-11-15 03:34:08 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -26,7 +26,7 @@ fi
 [[ $(twingate status) != *"online"* ]] && timeout 10 /usr/bin/twingate-notifier console
 
 # Mount SNAS setup
-mkdir -p /home/mnt/snas/sync
+sudo mkdir -p /home/mnt/snas/sync
 sudo mkdir /home/mnt/snas/setup -p
 [[ ! -f /home/mnt/snas/sync/MOUNT_CHECK ]] && sudo chown $USER: -R /home/mnt/snas/sync
 [[ ! -f /home/mnt/snas/setup/MOUNT_CHECK ]] && sudo chown $USER: -R /home/mnt/snas/setup
