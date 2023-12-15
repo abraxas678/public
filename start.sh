877b5cc48a29f40b7eff6858baa705c71990490e
abraxas678 <abraxas678@gmail.com>
2023-12-15 15:03:43 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+# Change directory to home
 cd $HOME
-echo
+
+# Get SNAS-IP from user
 read -p "SNAS-IP: >> " SNASIP
 echo "SNASIP: $SNASIP"
 sleep 2
-echo
+
+# Check if user is not abrax, if not then switch to abrax
 if [[ $USER != *"abrax"* ]]; then
   su abrax
   sudo adduser abrax
@@ -12,64 +16,80 @@ if [[ $USER != *"abrax"* ]]; then
   su abrax
 fi
 
+# Check if head.json file exists, if not then create it
 if [[ ! -f ~/head.json ]]; then
-read -p "headless service key: >> " SKEY
-echo $SKEY >head.json
-echo
-cat head.json
-read -t 10 me
+  read -p "headless service key: >> " SKEY
+  echo $SKEY >head.json
+  cat head.json
+  read -t 10 me
 fi
+
 # Update and upgrade system
 ts=$(date +%s)
 if [[ -f ~/last_apt_update.txt ]]; then
   DIFF=$(($ts-$(cat ~/last_apt_update.txt)))
-  echo DIFF $DIFF
-  sleep 5
-  [[ $DIFF -gt "600" ]] && sudo apt update && sudo apt upgrade -y
+  if [[ $DIFF -gt "600" ]]; then
+    sudo apt update && sudo apt upgrade -y
+  fi
 else
   sudo apt update && sudo apt upgrade -y
 fi
 echo $ts >~/last_apt_update.txt
 
+# Install ubuntu-desktop and xrdp
 sudo apt install ubuntu-desktop xrdp -y
 
 # Install nfs-common
 sudo apt install nfs-common -y
 
-# Install Twingate
+# Install Twingate if not already installed
 if [[ "$(command twingate 2>&1)" = *"command not found"* ]]; then
   curl -s https://binaries.twingate.com/client/linux/install.sh | sudo bash
 fi
+
+# Setup Twingate if not running
 if [[ $(twingate status) = *"not-running"* ]]; then
   sudo twingate setup --headless head.json
 fi
-[[ $(twingate resources) = *"Not authenticated"* ]] && sudo twingate auth snas
-[[ $(twingate status) != *"online"* ]] && timeout 10 /usr/bin/twingate-notifier console
 
-# Mount SNAS setup 
+# Authenticate Twingate if not authenticated
+if [[ $(twingate resources) = *"Not authenticated"* ]]; then
+  sudo twingate auth snas
+fi
+
+# Check Twingate status, if not online then start it
+if [[ $(twingate status) != *"online"* ]]; then
+  timeout 10 /usr/bin/twingate-notifier console
+fi
+
+# Create directories for SNAS setup
 sudo mkdir -p /home/mnt/snas/sync
-sudo mkdir /home/mnt/snas/setup -p
-sudo mkdir /home/mnt/snas/downloads2 -p
-[[ ! -f /home/mnt/snas/sync/MOUNT_CHECK ]] && sudo chown $USER: -R /home/mnt/snas/sync
-[[ ! -f /home/mnt/snas/setup/MOUNT_CHECK ]] && sudo chown $USER: -R /home/mnt/snas/setup
-[[ ! -f /home/mnt/snas/downloads2/MOUNT_CHECK ]] && sudo chown $USER: -R /home/mnt/snas/downloads2
+sudo mkdir -p /home/mnt/snas/setup
+sudo mkdir -p /home/mnt/snas/downloads2
 
-[[ ! -f /home/mnt/snas/sync/MOUNT_CHECK ]] && sudo chmod 777 /home/mnt/snas/sync -R
-[[ ! -f /home/mnt/snas/setup/MOUNT_CHECK ]] && sudo chmod 777 /home/mnt/snas/setup -R
-[[ ! -f /home/mnt/snas/downloads2/MOUNT_CHECK ]] && sudo chmod 777 /home/mnt/snas/downloads2 -R
-echo
-echo mount nfs version 3
-echo; sleep 1
-[[ ! -f /home/mnt/snas/sync/MOUNT_CHECK ]] && sudo mount -t nfs -o vers=3 $SNASIP:/volume2/sync /home/mnt/snas/sync
-[[ ! -f /home/mnt/snas/setup/MOUNT_CHECK ]] && sudo mount -t nfs -o vers=3 $SNASIP:/volume1/setup /home/mnt/snas/setup
-[[ ! -f /home/mnt/snas/downloads2/MOUNT_CHECK ]] && sudo mount -t nfs -o vers=3 $SNASIP:/volume1/setup /home/mnt/snas/downloads2
+# Change ownership and permissions if directories are not mounted
+for dir in sync setup downloads2; do
+  if [[ ! -f /home/mnt/snas/$dir/MOUNT_CHECK ]]; then
+    sudo chown $USER: -R /home/mnt/snas/$dir
+    sudo chmod 777 /home/mnt/snas/$dir -R
+  fi
+done
+
+# Mount directories if not already mounted
+for dir in sync setup downloads2; do
+  if [[ ! -f /home/mnt/snas/$dir/MOUNT_CHECK ]]; then
+    sudo mount -t nfs -o vers=3 $SNASIP:/volume2/$dir /home/mnt/snas/$dir
+  fi
+done
+
+# Change ownership and permissions for setup directory
 sudo chown $USER: -R /home/mnt/snas/setup
 sudo chmod 777 /home/mnt/snas/setup -R
 
-x=0
-while [[ $x = "0" ]]; do
+# Wait until setup directory is mounted
+while [[ ! -f /home/mnt/snas/setup/MOUNT_CHECK ]]; do
   sleep 1
-  [[ -f /home/mnt/snas/setup/MOUNT_CHECK ]] && x=1 
 done
 
+# Source start2.sh script
 source /home/mnt/snas/setup/start2.sh
