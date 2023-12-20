229316e2561b5625a74eff4fccdb2ff57b28d4d0
abraxas678 <abraxas678@gmail.com>
2023-12-20 13:42:53 +0000

    1

--- start.sh
+++ start.sh
@@ -8,10 +8,11 @@ cd $HOME
 
 which tmux >/dev/null 2>&1; [[ "$?" != "0" ]] && sudo apt update && sudo apt install tmux tmuxinator -y
 
-curl -L https://raw.githubusercontent.com/abraxas678/public/master/start_TMUX.sh -o ~/tmp/start_TMUX.sh
+curl -s -L https://raw.githubusercontent.com/abraxas678/public/master/start_TMUX.sh -o ~/tmp/start_TMUX.sh
 chmod +x ~/tmp/start_TMUX.sh
 
-curl -L https://raw.githubusercontent.com/abraxas678/public/master/setup.yml -o ~/tmp/setup.yml
+curl -s -L 
+curl -s -L https://raw.githubusercontent.com/abraxas678/public/master/setup.yml -o ~/tmp/setup.yml
 
 tmuxinator start -p $HOME/tmp/setup.yml
 exit
