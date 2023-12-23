4238c27d950f59d38b3d143c6dd382e6e1d18d11
abraxas678 <abraxas678@gmail.com>
2023-12-23 23:14:56 +0100

    1

---
+++ machine.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd $HOME
+read -p "new hostname: >> " HOSTNAME
+HOSTNAME_OLD=$(hostname)
+sudo hostnamectl set-hostname "$HOSTNAME"
+sudo echo "$HOSTNAME" >$HOME/hostname
+sudo mv $HOME/hostna; e /etc/hostname
+echo; echo "/etc/hostname: "; cat /etc/hostname; echo
+sudo sed -i "s/$HOSTNAME_OLD/$HOSTNAME/g" /etc/hosts
+echo; echo "/etc/hosts: "; cat /etc/hosts; echo
+cd $HOME
+wget https://raw.githubusercontent.com/abraxas678/public/master/wsl.conf
+#cp $HOME/server_setup/wsl.conf $HOME
+sed -i "s/CHANGEHOSTNAME/$HOSTNAME/g" $HOME/wsl.conf
+sudo mv wsl.conf /etc/
