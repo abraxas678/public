31909c92d0c99a686c3ce89cad7ac2e38757e65b
abraxas678 <abraxas678@gmail.com>
2023-12-23 15:28:47 +0100

    Update start_TMUX.sh

--- start_TMUX.sh
+++ start_TMUX.sh
@@ -4,10 +4,10 @@
 #tmux send-keys -t 0 'start.sh'
 #            - tmux send-keys -t 1 'clear' C-m
 
-tmux send-keys -t 1 'clear' C-m
-tmux send-keys -t 0 'clear' C-m
-tmux send-keys -t 0 'clear; echo -e "\033[1;34mSETUP NEW MACHINE\033[0m"' C-m
-tmux send-keys -t 0 'start_TMUX.sh'
+#tmux send-keys -t 1 'clear' C-m
+#tmux send-keys -t 0 'clear' C-m
+#tmux send-keys -t 0 'clear; echo -e "\033[1;34mSETUP NEW MACHINE\033[0m"' C-m
+#tmux send-keys -t 0 'start_TMUX.sh'
 
 MYPWD=$PWD
 # Change directory to home
