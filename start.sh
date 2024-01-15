bb8249daac7bf347f050bd03829b21631c4f6809
abraxas678 <abraxas678@gmail.com>
2024-01-15 22:56:08 +0100

    1

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ cd $HOME/tmp
 sudo apt install curl git wget -y
 git config --global user.email "abraxas678@gmail.com"
 git config --global user.name "abraxas678"
+
 #which tmux >/dev/null 2>&1; [[ "$?" != "0" ]] && sudo apt update && sudo apt install tmux tmuxinator -y
 
 #curl -s -L https://raw.githubusercontent.com/abraxas678/public/master/start_TMUX.sh -o ~/tmp/start_TMUX.sh
@@ -58,12 +59,12 @@ if [[ $USER != *"abrax"* ]]; then
 fi
 
 # Check if head.json file exists, if not then create it
-if [[ ! -f ~/head.json ]]; then
-  read -p "headless service key: >> " SKEY
-  echo $SKEY >head.json
-  cat head.json
-  read -t 10 me
-fi
+#if [[ ! -f ~/head.json ]]; then
+#  read -p "headless service key: >> " SKEY
+#  echo $SKEY >head.json
+#  cat head.json
+#  read -t 10 me
+#fi
 
 # Update and upgrade system
 ts=$(date +%s)
@@ -84,32 +85,33 @@ sudo apt install ubuntu-desktop xrdp -y
 sudo apt install nfs-common -y
 
 # Install Twingate if not already installed
-if [[ "$(command twingate 2>&1)" = *"command not found"* ]]; then
-  curl -s https://binaries.twingate.com/client/linux/install.sh | sudo bash
-fi
+#if [[ "$(command twingate 2>&1)" = *"command not found"* ]]; then
+#  curl -s https://binaries.twingate.com/client/linux/install.sh | sudo bash
+#fi
 
 # Setup Twingate if not running
-if [[ $(twingate status) = *"not-running"* ]]; then
-  sudo twingate setup --headless head.json
-fi
+#if [[ $(twingate status) = *"not-running"* ]]; then
+#  sudo twingate setup --headless head.json
+#fi
 
 # Authenticate Twingate if not authenticated
-if [[ $(twingate resources) = *"Not authenticated"* ]]; then
-  sudo twingate auth snas
-fi
+#if [[ $(twingate resources) = *"Not authenticated"* ]]; then
+#  sudo twingate auth snas
+#fi
 
 # Check Twingate status, if not online then start it
-if [[ $(twingate status) != *"online"* ]]; then
-  timeout 10 /usr/bin/twingate-notifier console
-fi
+#if [[ $(twingate status) != *"online"* ]]; then
+#  timeout 10 /usr/bin/twingate-notifier console
+#fi
 
 # Create directories for SNAS setup
-sudo mkdir -p /home/mnt/snas/sync
+#sudo mkdir -p /home/mnt/snas/sync
 sudo mkdir -p /home/mnt/snas/setup
-sudo mkdir -p /home/mnt/snas/downloads2
+#sudo mkdir -p /home/mnt/snas/downloads2
 
 # Change ownership and permissions if directories are not mounted
-for dir in sync setup downloads2; do
+#for dir in sync setup downloads2; do
+for dir in setup; do
   if [[ ! -f /home/mnt/snas/$dir/MOUNT_CHECK ]]; then
     sudo chown $USER: -R /home/mnt/snas/$dir
     sudo chmod 777 /home/mnt/snas/$dir -R
@@ -120,7 +122,8 @@ curl -L https://raw.githubusercontent.com/abraxas678/public/master/mount.sh -o m
 source mount.sh
 
 # Mount directories if not already mounted
-for dir in sync setup downloads2; do
+#for dir in sync setup downloads2; do
+for dir in setup; do
   if [[ ! -f /home/mnt/snas/$dir/MOUNT_CHECK ]]; then
     sudo mount -t nfs -o vers=3 $SNAS_IP:/volume2/$dir /home/mnt/snas/$dir
     sudo mount -t nfs -o vers=3 $SNAS_IP:/volume1/$dir /home/mnt/snas/$dir
@@ -128,11 +131,12 @@ for dir in sync setup downloads2; do
 done
 
 # Change ownership and permissions for setup directory
-sudo chown $USER: -R /home/mnt/snas/setup
-sudo chmod 777 /home/mnt/snas/setup -R
+#sudo chown $USER: -R /home/mnt/snas/setup
+#sudo chmod 777 /home/mnt/snas/setup -R
 
 # Wait until setup directory is mounted
 while [[ ! -f /home/mnt/snas/setup/MOUNT_CHECK ]]; do
+  echo "checking mount"
   sleep 1
 done
 
