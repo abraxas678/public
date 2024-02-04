ff96294a13999321e997c3e69c2f4dfaa971665a
abraxas678 <abraxas678@gmail.com>
2024-02-04 18:02:39 +0000

    1

---
+++ countdown.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+countdown() {
+    if [ -z "$1" ]; then
+        echo "No argument provided. Please provide a number to count down from."
+        exit 1
+    fi
+
+    tput civis
+    for ((i=$1; i>0; i--)); do
+        if (( i > $1*66/100 )); then
+            echo -ne "\033[0;32m$i\033[0m\r"
+        elif (( i > $1*33/100 )); then
+            echo -ne "\033[0;33m$i\033[0m\r"
+        else
+            echo -ne "\033[0;31m$i\033[0m\r"
+        fi
+        sleep 1
+        echo -ne "\033[0K"
+    done
+    echo -e "\033[0m"
+    tput cnorm
+}
+
+countdown $1
