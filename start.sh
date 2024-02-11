01db4c8e434643edeca940cd348745be5da8cf05
abraxas678 <abraxas678@gmail.com>
2024-02-11 04:14:28 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 cd $HOME
-echo v0.98
+echo v0.99
 read -t 5 me
 
 mkdir ~/tmp -p
@@ -58,11 +58,19 @@ echo user1
 TASK "CHECK: USER = abrax? "
 # Check if user is not abrax, if not then switch to abrax
 if [[ $USER != *"abrax"* ]]; then
-  su abrax
-  sudo adduser abrax
-  sudo usermod -aG sudo abrax
-  su abrax
-  exit
+  if [[ $USER = *"root"* ]]; then
+    su abrax
+    adduser abrax
+    usermod -aG sudo abrax
+    su abrax
+    exit
+  else
+    su abrax
+    sudo adduser abrax
+    sudo usermod -aG sudo abrax
+    su abrax
+    exit
+  fi
 fi
 echo user2
 
