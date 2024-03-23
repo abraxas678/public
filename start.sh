6e1bb73af8f81d23301a7536751a1e369f9cc479
abraxas678 <abraxas678@gmail.com>
2024-03-23 04:05:00 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,20 @@ clear
 cd $HOME
 echo version: NEWv0.2
 read -t 2 me
+echo
+echo check_dns
+cd $HOME
+ping -c 1 google.com >/dev/null && echo "Online" || echo "Offline"
+ping -c 1 google.com >/dev/null && ONL=1 || ONL=0
+if [[ $ONL = "0" ]]; then
+  CHECK=$(cat /etc/resolv.conf)
+  if [[ $CHECK != *"8.8.8.8"* ]] ; then
+    echo nameserver 8.8.8.8 >~/resolv.conf
+    cat /etc/resolv.conf>>~/resolv.conf
+    sudo mv ~/resolv.conf /etc/
+  fi
+ping -c 1 google.com >/dev/null && echo "Online" || echo "Offline"
+fi
 
 read -p "RCLONE_CONFIG_PASS >> " MYPW
 export RCLONE_CONFIG_PASS="$MYPW"
