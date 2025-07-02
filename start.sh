aab8f893e786820b97c7e4d5dc5acde6d96a6bb0
abrax <abrax@core>
2025-07-02 05:22:39 +0200

    1

--- start.sh
+++ start.sh
@@ -16,6 +16,27 @@ instme() {
    fi
 
 }
+instgit() {
+  curl -sL "https://api.github.com/repos/$1/$2/releases/latest" | gron | grep browser_download_url | grep deb | sed "s/.*browser_download_url = //" | sed "s/^\"//" | sed "s/\";$//" | grep -v arm >res
+  COUNT=$(cat res | wc -l)
+  if [[ $COUNT != 0 ]]; then
+    echo -n ""
+  else
+    curl -sL "https://api.github.com/repos/$1/$2/releases/latest" | gron | grep browser_download_url | sed "s/.*browser_download_url = //" | sed "s/^\"//" | sed "s/\";$//" | grep -v arm >res
+    COUNT=$(cat res | wc -l)
+   fi
+   RES="$(cat res | sed 's/.*download//'| fzf)"
+   if [[ $COUNT != 0 ]]; then
+     cat res | grep "$RES" | xsel -b
+     cat res | grep "$RES"
+     $MYSUDO apt update
+     wget $(cat res | grep "$RES")
+     $MYSUDO apt install -y  ./$(basename $(cat res | grep "$RES"))
+   else
+     cat res
+     echo no
+   fi
+}
 
 #[[ ! -f /media/abrax/KopiaBackup/chez.tar.cpt ]] && echo "/media/abrax/KopiaBackup/chez.tar.cpt" && exit
 mkdir -p ~/.ssh
@@ -24,7 +45,10 @@ mkdir -p ~/.config/chezmoi
 #sleep 3
 
 #instme ccrypt
-instme fd
+instme fd-find
+instme gron
+instme fzf
+instgit binwiederhier pcopy
 
 cd ~/.config/chezmoi
 #if [[ ! -f ~/.ssh/bws.dat ]]; then
