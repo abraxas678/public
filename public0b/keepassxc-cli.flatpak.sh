bffcfdc26b103ca795f724bfbad680aa6b48bb82
abraxas678 <abraxas678@gmail.com>
2025-10-05 20:31:28 +0200

    Fixed bug in login functionality and updated related test cases AIANS

--- public0b/keepassxc-cli.flatpak.sh
+++
@@ -1,1 +0,0 @@
-flatpak run --branch=stable --arch=x86_64 --command=keepassxc-cli --file-forwarding org.keepassxc.KeePassXC @@ %f @@
--- public0b/onlykey-cli_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt update && sudo apt upgrade
-sudo apt install python3-pip python3-tk libusb-1.0-0-dev libudev-dev
-pip3 install onlykey
-wget https://raw.githubusercontent.com/trustcrypto/trustcrypto.github.io/pages/49-onlykey.rules
-sudo cp 49-onlykey.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules && udevadm trigger
